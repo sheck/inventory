a69df6ac957f555c61041aa11cf7c51cdfbd260d
Nick Sheck <nsheck@gmail.com>
2015-08-21 12:54:48 -0700

    Unfinished feature spec

---
+++ spec/features/user_adds_items_to_list_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "User adds items to list" do
+  scenario "successfully" do
+    fill_in "List name", with: "Working vehicles"
+    click_on "Create list"
+
+    expect(list).to have_content "Working vehicles"
+    expect(list).to have_content "Stair car"
+  end
+end
