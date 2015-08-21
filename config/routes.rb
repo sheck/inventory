f67d2d895fee9350f565b67c758e0ba299695f65
Nick Sheck <nsheck@gmail.com>
2015-08-21 12:12:38 -0700

    rails g resource List name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lists
   root 'items#index'
   resources :items
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820145914) do
+ActiveRecord::Schema.define(version: 20150821191225) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150820145914) do
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
