be3d918a88635b465957df56747247ed30507850
Nick Sheck <nsheck@gmail.com>
2015-08-29 21:34:54 -0700

    Ensure list assignment is scoped to user
    Closes #2

--- spec/controllers/list_assignments_controller_spec.rb
+++ spec/controllers/list_assignments_controller_spec.rb
@@ -8,7 +8,6 @@ describe ListAssignmentsController do
       item = create(:item, user: user)
       list1 = create(:list, user: user)
       list2 = create(:list, user: user, name: "Music equipment")
-      # list3 = create(:list, user: user, name: "Cables")
       create(:list_assignment, item: item, list: list1)
       sign_in_as(user)
 
@@ -16,9 +15,9 @@ describe ListAssignmentsController do
 
       expect(assigns(:lists)).to_not include(list1)
       expect(assigns(:lists)).to include(list2)
-      # expect(assigns(:lists)).to include(list3)
     end
   end
+
   describe "DELETE #destroy" do
     it "only deletes if assignment belongs to user" do
       user1 = create(:user)
@@ -30,6 +29,31 @@ describe ListAssignmentsController do
 
       expect {
         delete :destroy, { id: list_assignment.id }
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe "POST #create" do
+    it "only creates if item belongs to user" do
+      user1 = create(:user)
+      user2 = create(:user)
+      item = create(:item, user: user1)
+      list = create(:list, user: user2)
+      sign_in_as(user2)
+
+      expect {
+        post :create, { item_id: item.id, list_id: list.id }
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+    it "only creates if list belogs to user" do
+      user1 = create(:user)
+      user2 = create(:user)
+      item = create(:item, user: user2)
+      list = create(:list, user: user1)
+      sign_in_as(user2)
+
+      expect {
+        post :create, { item_id: item.id, list_id: list.id }
       }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
