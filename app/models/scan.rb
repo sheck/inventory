4bb55ff5e42cf97a29275655f63c360704065341
Nick Sheck <nsheck@gmail.com>
2015-11-08 15:29:44 -0700

    Break up Scan method a bit
    Cleanup

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,8 +1,8 @@
 class Scan
   include ActiveModel::Model
 
-  attr_accessor :user, :barcode_image, :list_ids, :upc
-  attr_reader :item, :query_error
+  attr_accessor :user, :barcode_image, :list_ids
+  attr_reader :item, :query_error, :upc
 
   validates_presence_of :user
   validates_presence_of :barcode_image
@@ -10,23 +10,21 @@ class Scan
   validates_absence_of :query_error
 
   def save
-    @upc = parse_barcode
-    @query = query_amazon
-    @query_error = @query.try(:error)
+    parse_barcode
+    query_amazon
     if valid?
-      az_item = @query.items.first
-      @item = user.items.new name: az_item.get("ItemAttributes/Title"), list_ids: list_ids.reject(&:empty?)
-      if az_item.get('LargeImage/URL')
-        @item.photo = URI.parse(az_item.get('LargeImage/URL'))
-      end
+      @item = user.items.new name: title, list_ids: lists
+      add_photo_if_available
       @item.save
     end
   end
 
+  private
+
   def parse_barcode
     if barcode_image
       barcode = ZBar::Image.from_jpeg(barcode_image).process
-      barcode.try(:first).try(:data)
+      @upc = barcode.try(:first).try(:data)
     end
   end
 
@@ -39,7 +37,27 @@ class Scan
         :AWS_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
         :AWS_secret_key => ENV["AWS_SECRET_ACCESS_KEY"]
       }
-      Amazon::Ecs.item_search(@upc, response_group: 'Medium', search_index: 'All')
+      @query = Amazon::Ecs.item_search(@upc, response_group: 'Medium',
+                                       search_index: 'All')
+      @query_error = @query.try(:error)
+    end
+  end
+
+  def title
+    query_item.get("ItemAttributes/Title")
+  end
+
+  def lists
+    list_ids.reject(&:empty?)
+  end
+
+  def query_item
+    @query.items.first
+  end
+
+  def add_photo_if_available
+    if query_item.get('LargeImage/URL')
+      @item.photo = URI.parse(query_item.get('LargeImage/URL'))
     end
   end
 end
--- spec/models/scan_spec.rb
+++ spec/models/scan_spec.rb
@@ -1,11 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Scan do
   describe "#save" do
-    # TODO: break up this test and cover the amazon query method. should not be
-    #       using vcr for testing this method.
-    it 'creates an item from a barcode image' do
-      VCR.use_cassette('barcode') do |cassette|
+    it "creates an item from a barcode image" do
+      VCR.use_cassette("barcode") do |cassette|
         Timecop.freeze(cassette.originally_recorded_at || Time.now) do
           barcode = File.open("spec/support/barcode.jpg", mode: "rb")
           scan = Scan.new(barcode_image: barcode, list_ids: [], user: create(:user))
@@ -19,17 +17,6 @@ describe Scan do
           expect(item.photo).to_not be_blank
         end
       end
-    end
-  end
-
-  describe "#parse_barcode" do
-    it 'accepts an image and returns a barcode' do
-      barcode = File.open("spec/support/barcode.jpg", mode: "rb")
-      scan = Scan.new(barcode_image: barcode, list_ids: [], user: create(:user))
-
-      result = scan.parse_barcode
-
-      expect(result).to eq("9781595554697")
     end
   end
 end
