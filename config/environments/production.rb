58596cfa0f59c11669db096a514ba87540dec71d
Nick Sheck <nsheck@gmail.com>
2015-11-08 21:51:01 -0700

    Setup sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,15 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  # Sendgrid
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
